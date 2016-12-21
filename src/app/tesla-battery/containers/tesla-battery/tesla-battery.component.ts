4ab097d4e4130ad91196210bb0ad3bbfb5508c62
Alper Gokcehan <mail@alperg.com>
2016-12-20 19:34:54 -0500

    stats

--- src/app/tesla-battery/containers/tesla-battery/tesla-battery.component.ts
+++ src/app/tesla-battery/containers/tesla-battery/tesla-battery.component.ts
@@ -3,6 +3,8 @@
  */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Stat } from '../../models/stat.interface';
+import { BatteryService } from '../../tesla-battery.service';
 
 @Component({
   selector: 'tesla-battery',
@@ -10,6 +12,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
     <form class="tesla-battery" [formGroup]="tesla">
       <h1>{{ title }}</h1>
       <tesla-car [wheelsize]="tesla.get('config.wheels').value"></tesla-car>
+      <tesla-stats [stats]="stats"></tesla-stats>
       <div class="tesla-battery__notice">
         <p>
           The actual amount of range that you experience will vary based 
@@ -28,20 +31,37 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class TeslaBatteryComponent implements OnInit {
   
-  title: string = 'Range Per Charge';
-  tesla: FormGroup;
+    title: string = 'Range Per Charge';
+    models: any;
+    stats: Stat[];
+    tesla: FormGroup;
+  
+    private results: Array<String> = ['60', '60D', '75', '75D', '90D', 'P100D'];
+    private calculateStats(models, value): Stat[]  {
+        return models.map(model => {
+            const { speed, temperature, climate, wheels } = value;
+            const miles = this.models[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+            return {
+                model,
+                miles
+            };
+        });
+    }
+    constructor(public fb: FormBuilder, private batteryService: BatteryService) {}
 
-  constructor(public fb: FormBuilder) {}
+    ngOnInit() {
+        this.models = this.batteryService.getModelData();
 
-  ngOnInit() {
-    this.tesla = this.fb.group({
-      config: this.fb.group({
-        speed: 55,
-        temperature: 20,
-        climate: true,
-        wheels: 19
-      })
-    });
-  }
+        this.tesla = this.fb.group({
+            config: this.fb.group({
+                speed: 55,
+                temperature: 20,
+                climate: true,
+                wheels: 19
+            })
+        });
+
+        this.stats = this.calculateStats(this.results, this.tesla.controls['config'].value);
+    }
 
 }
